65a43956f087a9c9e3e4d44c5f80dce29fca5494
Ekaterina Biryukova <kateabr@yandex.ru>
2017-10-26 23:43:56 +0300

    Нужно отрегулировать шаг

---
+++ LSystems/paintcanvas.cpp
@@ -0,0 +1,6 @@
+#include "paintcanvas.h"
+
+paintcanvas::paintcanvas()
+{
+
+}
--- LSystems/paintcanvas.h
+++ LSystems/paintcanvas.h
@@ -1,4 +1,11 @@
 #ifndef PAINTCANVAS_H
 #define PAINTCANVAS_H
 
+
+class paintcanvas
+{
+public:
+    paintcanvas();
+};
+
 #endif // PAINTCANVAS_H
